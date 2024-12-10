f723cfbe15f3569f442f055ec56310fbdc22f447
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2024-12-10 11:51:54 +0530

    OOPS questions

---
+++ OOPS/Ques_4.java
@@ -0,0 +1,34 @@
+//Output
+
+abstract class Car{
+    static {
+        System.out.print("1");
+    }
+
+    public Car(String name){
+        super();
+        System.out.print("2");
+    }
+
+    {
+        System.out.print("3");
+    }
+}
+
+public class BlueCar extends Car{
+    {
+        System.out.print("4");
+    }
+
+    public BlueCar(){
+        super("blue");
+        System.out.print("5");
+    }
+
+}
+
+public class Ques_4{
+    public static void main(String[] gears) {
+        new BlueCar();
+    }
+}
