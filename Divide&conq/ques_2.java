27896661463a47e8b0277fd927d8843a27452d9d
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2024-12-24 16:30:17 +0530

    hard ques

--- Divide&conq/ques_2.java
+++ Divide&conq/ques_2.java
@@ -10,5 +10,24 @@ n == nums.length
 */
 
 public class ques_2{
-    public static 
+    public static int majorityElement(int[] nums){
+        int majorityCount = nums.length/2;
+        for(int i = 0; i < nums.length; i++){
+            int count = 0;
+            for(int j = 0; j < nums.length; j++){
+                if(nums[j] == nums[i]){
+                    count += 1;
+                }
+            }
+            if(count > majorityCount){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {2,2,1,1,1,2,2};
+        System.out.println(majorityElement(nums));
+    }
 }
