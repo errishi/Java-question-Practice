1f3b6a1f83e905ec31a6fae5eedba020dd6907f4
Rishikesh Singh <er.rishikeshsingh0@gmail.com>
2026-05-26 12:16:05 +0530

    queue

---
+++ Queue/ConnectRopeMinCost.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+
+// Connect n ropes with minimum cost
+// Given are N ropes of different lengths, the task is to connect these ropes into one rope with minimum cost, such that the cost to connect two ropes is equal to the sum of their lengths.
+
+// Sample Input 1: N = 4, arr = [4 3 2 6]
+// Sample Output 1: 29
+// Sample Input 2: N = 2, arr = [1 2 3]
+// Sample Output 2: 9
+
+public class ConnectRopeMinCost{
+    static int minCost(int arr[], int n){
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+
+        for (int i = 0; i < n; i++) {
+            pq.add(arr[i]);
+        }
+
+        int res = 0;
+        while(pq.size() > 1){
+            int first = pq.poll();
+            int second = pq.poll();
+            res += first + second;
+            pq.add(first + second);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int len[] = {4, 3, 2, 6};
+        int size = len.length;
+        System.out.println("Total cost of connecting" + "ropes is " + minCost(len, size));
+    }
+}
---
+++ Queue/MaxSubarraySize.java
@@ -0,0 +1,38 @@
+
+import java.util.*;
+
+// Maximum of all subarrays of size k
+// We have an array arr[] of size N and an integer K. Find the maximum for each and every contiguous subarray of size K.
+// Sample Input 1:N=9, K=3 arr= 1 2 3 1 4 5 2 3 6
+// Sample Output 1: 3 3 4 5 5 5 6
+
+public class MaxSubarraySize{
+    static void printMax(int arr[], int n, int k){
+        Deque<Integer> Qi = new LinkedList<Integer>();
+        int i;
+        for (i = 0; i < k; ++i) {
+            while(!Qi.isEmpty() && arr[i] >= arr[Qi.peekLast()]){
+                Qi.removeLast();
+            }
+            Qi.addLast(i);;
+        }
+
+        for(; i < n; ++i){
+            System.out.println(arr[Qi.peek()] + " ");
+            while((!Qi.isEmpty()) && Qi.peek() <= i-k){
+                Qi.removeFirst();
+            }
+            while((!Qi.isEmpty()) && arr[i] >= arr[Qi.peekLast()]){
+                Qi.removeLast();
+            }
+            Qi.addLast(i);
+        }
+        System.out.println(arr[Qi.peek()]);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {12, 1, 78, 90, 57, 89, 56};
+        int k = 3;
+        printMax(arr, arr.length, k);
+    }
+}
